3d31627ee3b7089e80e38bd61212bd09707fddc1
lena0428 <139748662+lena0428@users.noreply.github.com>
2024-07-04 13:35:47 -0700

    add variable name for app's name and used it in the Text

--- App.js
+++ App.js
@@ -2,10 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const appName = "lena_first_app";
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Hello World</Text>
+      <Text>Welcome to {appName}.</Text>
       <StatusBar style="auto" />
     </View>
   );
