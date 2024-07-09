3c6a7238bde878f0a1f2847d51b50a7a6f24f05c
lena0428 <139748662+lena0428@users.noreply.github.com>
2024-07-09 14:36:37 -0700

    Activity - Modal

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Text} from 'react-native';
+import { StyleSheet, View, Text, Button } from 'react-native';
 import Header from './Components/Header';
 import Input from './Components/Input';
 import { useState } from 'react';
@@ -8,21 +8,25 @@ import { useState } from 'react';
 export default function App() {
   const appName = "lena_first_app";
   const [receivedText, setReceivedText] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
   // set callback function to handle the received data
   function handleInputData(data) {
     console.log('callback fn called with:', data);
     // set the receive data to the state
     setReceivedText(data);
+    // hide the modal
+    setModalVisible(false);
   }
   return (
     <View style={styles.container}>
       {/* use a prop to pass appName to Header */}
-      <Header app_name = {appName} theme="dark"> 
-      {/* <Text>Children1</Text>
+      <Header app_name={appName} theme="dark">
+        {/* <Text>Children1</Text>
       <Text>Children2</Text> */}
       </Header>
-      <Input inputHandler = {handleInputData}/>
+      <Input inputHandler={handleInputData} isModalVisible={modalVisible} />
       <Text>Received: {receivedText}</Text>
+      <Button title='Add a goal' onPress={() => setModalVisible(true)} />
       <StatusBar style="auto" />
     </View>
   );
--- Components/Input.js
+++ Components/Input.js
@@ -1,35 +1,46 @@
-import { TextInput, Text, Button } from 'react-native'
+import { StyleSheet, TextInput, Text, Button, View, Modal } from 'react-native'
 import React from 'react'
 import { useState } from 'react';
 
 // update Input to receive a prop
-const Input = ({inputHandler}) => {
+const Input = (props) => {
     const [isBlurred, setIsBlurred] = useState(false);
     const [text, setText] = useState('');
     function handleConfirm() {
         console.log('user type:', text);
         // call the received prop callback fn
-        inputHandler(text);
+        props.inputHandler(text);
     }
     return (
-        <>
-            <TextInput
-                style={{ height: 40 }}
-                placeholder="Type here to translate!"
-                onChangeText={(newText) => {
-                    setText(newText);
-                    if (isBlurred) {
-                        setIsBlurred(false);
-                    }
-                }}
-                onBlur={() => setIsBlurred(true)}
-                onFocus={() => setIsBlurred(false)}
-                value={text}>
-            </TextInput>
-            {isBlurred && <Text>Thank you</Text>}
-            <Button title='Confirm' onPress={handleConfirm}>Confirm</Button>
-        </>
+        <Modal animationType='slide' visible={props.isModalVisible}>
+            <View style={styles.container}>
+                <TextInput
+                    style={{ height: 40 }}
+                    placeholder="Type here to translate!"
+                    onChangeText={(newText) => {
+                        setText(newText);
+                        if (isBlurred) {
+                            setIsBlurred(false);
+                        }
+                    }}
+                    onBlur={() => setIsBlurred(true)}
+                    onFocus={() => setIsBlurred(false)}
+                    value={text}>
+                </TextInput>
+                {isBlurred && <Text>Thank you</Text>}
+                <Button title='Confirm' onPress={handleConfirm}>Confirm</Button>
+            </View >
+        </Modal>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
 export default Input
