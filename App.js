ae15952c6a498b2677b2ddbfcdd60126d8ca1682
lena0428 <139748662+lena0428@users.noreply.github.com>
2024-07-09 17:08:05 -0700

    add a button called cancel to iput component, change button style and clear the textinput after user presses confirm or cancel buttons in input.js

--- App.js
+++ App.js
@@ -4,30 +4,43 @@ import Header from './Components/Header';
 import Input from './Components/Input';
 import { useState } from 'react';
 
-
 export default function App() {
   const appName = "lena_first_app";
   const [receivedText, setReceivedText] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
-  // set callback function to handle the received data
+
+  // Callback function to handle the received data
   function handleInputData(data) {
-    console.log('callback fn called with:', data);
-    // set the receive data to the state
+    console.log('Callback function called with:', data);
     setReceivedText(data);
-    // hide the modal
     setModalVisible(false);
   }
+
+  const handleConfirm = () => {
+    setModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setModalVisible(false);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      {/* use a prop to pass appName to Header */}
       <View style={styles.topContainer}>
         <Header app_name={appName} theme="dark" />
         <Button title='Add a goal' onPress={() => setModalVisible(true)} />
       </View>
-      {/* <Text>Children1</Text>
-      <Text>Children2</Text> */}
-      <Input inputHandler={handleInputData} isModalVisible={modalVisible} />
-      <View style={styles.bottomContainer}><Text style={styles.textStyle}>Received: {receivedText}</Text></View>
+      <Input 
+        inputHandler={handleInputData} 
+        isModalVisible={modalVisible} 
+        onConfirm={handleConfirm}
+        onCancel={handleCancel}
+      />
+      <View style={styles.bottomContainer}>
+        <View style={styles.textContainer}>
+          <Text style={styles.textStyle}>Received: {receivedText}</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -37,16 +50,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
   textStyle: {
     color: 'darkmagenta',
+    padding: 10,
+  },
+  textContainer: {
+    backgroundColor: 'grey',
+    borderRadius: 10,
+    padding: 5,
   },
   topContainer: {
     flex: 1,
     backgroundColor: 'red',
     alignItems: 'center',
+    justifyContent: 'center',
   },
-  bottomContainer: { flex: 4, backgroundColor: 'lightblue', alignItems: 'center'}
+  bottomContainer: {
+    flex: 4,
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
