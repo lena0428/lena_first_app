2bee84b8512f34ea8b598e2b553244a34191de45
Your Name <your.email@example.com>
2024-07-30 14:21:34 -0700

    submit signup screen

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import Home from './Components/Home';
 import GoalDetails from './Components/GoalDetails';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Button } from 'react-native';
+import Login from './Components/Login';
+import Signup from './Components/Signup';
 
 const Stack = createNativeStackNavigator();
 
@@ -13,7 +14,9 @@ const headerStyle = {
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName='Signup'>
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Login" component={Login} />
         <Stack.Screen
           name="Home"
           component={Home}
--- Firebase/firebaseSetup.js
+++ Firebase/firebaseSetup.js
@@ -2,6 +2,9 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeAuth , getReactNativePersistence} from 'firebase/auth'
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,3 +33,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getFirestore(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
